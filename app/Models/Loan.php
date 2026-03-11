dcba0d3b779a55f05f15cde4705b5eac6647e868
Chaimae <chaimaekaz05@gmail.com>
2026-03-11 11:51:30 +0000

    Add Loan model and migration

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -3,11 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Loan extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'borrower_name',
         'borrower_email',
